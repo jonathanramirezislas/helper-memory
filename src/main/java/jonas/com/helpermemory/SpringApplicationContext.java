789e12a13b311c5b43b066d7551f325ebc3e9095
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-02-06 14:08:22 -0600

    SpringApplicationContext

---
+++ src/main/java/jonas/com/helpermemory/SpringApplicationContext.java
@@ -0,0 +1,19 @@
+package jonas.com.helpermemory;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+//this class allows to access to out Context so we can access to classes like Authentication that are not a bean 
+public class SpringApplicationContext implements ApplicationContextAware {
+
+    private static ApplicationContext CONTEXT;
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        CONTEXT = applicationContext;
+    }
+
+    public static Object getBean(String beanName) {
+        return CONTEXT.getBean(beanName);
+    }
+}
